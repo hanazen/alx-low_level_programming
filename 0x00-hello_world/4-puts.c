f1cccfb0971355486795bac8b83030ed96c2f12e
hanazen <hanazenabu@gmail.com>
2021-06-17 12:11:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point
- *first 
- * Return: 0
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
